1e19afea652255df4b9742aff2e41931f3e6d6b2
Mr. Burger <bburger@d155.org>
2021-01-12 19:38:51 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // Create a 2D array with [rows][columns] and automatically fills it with all
+    // 0's by default
+    int[][] abc = new int[3][5];
+    abc[0][0] = 1;
+    abc[2][4] = 9;
+    // loop through all the rows and columns
+    for (int r = 0; r < abc.length; r++) {
+      for (int c = 0; c < abc[0].length; c++) {
+        System.out.print(abc[r][c] + "\t");
+      }
+      System.out.println();
+    }
+    System.out.println("=======================================");
+    // Create and fill a 2D array with an initializer list
+    int[][] xyz = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 } };
+
+    // loop through all the rows and columns
+    for (int r = 0; r < xyz.length; r++) {
+      for (int c = 0; c < xyz[0].length; c++) {
+        System.out.print(xyz[r][c] + "\t");
+      }
+      System.out.println();
+    }
   }
 }
